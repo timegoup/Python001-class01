97178ae9ff17c6821d0cf5f4c7a132b60749f371
wushicheng <1961384503@qq.com>
2020-06-30 15:58:43 +0800

    第二周提交第二题作业

---
+++ week02/week02-02.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+
+try:
+    browser = webdriver.Chrome()
+    # 需要安装chrome driver, 和浏览器版本保持一致
+    # http://chromedriver.storage.googleapis.com/index.html
+    
+    browser.get('https://shimo.im/login?from=home')
+    time.sleep(3)
+
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div/input').send_keys('1961384503@qq.com')
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys('test123test456')
+    time.sleep(1)
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/button').click()
+
+    #cookies = browser.get_cookies() # 获取cookies
+    #print(cookies)
+    time.sleep(3)
+
+except Exception as e:
+    print(e)
+finally:    
+    browser.close()
+    
