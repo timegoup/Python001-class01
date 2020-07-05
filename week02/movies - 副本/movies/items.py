f52091e40c9d27db0ea0b340d83342328e71f157
wushicheng <1961384503@qq.com>
2020-07-05 22:30:53 +0800

    第二周第二次提交

---
+++ week02/movies - 副本/movies/items.py
@@ -0,0 +1,21 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MoviesItem(scrapy.Item):
+    # define the fields for your item here like:
+    #电影名称
+    name = scrapy.Field()
+    #电影类型
+    description = scrapy.Field()
+    #电影上映时间
+    releasetime = scrapy.Field()
+    #电影评分
+    score = scrapy.Field()
+
+
+    
